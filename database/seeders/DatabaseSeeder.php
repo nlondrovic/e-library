0770822ed15c0b53e8fb8a4984345bbad07d5845
jasmguli <nikolalondrovic41@gmail.com>
2022-07-10 17:49:32 +0200

    Changed order of calling seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RoleSeeder::class,
-            UsersTableSeeder::class
+            UsersSeeder::class
         ]);
     }
 }
