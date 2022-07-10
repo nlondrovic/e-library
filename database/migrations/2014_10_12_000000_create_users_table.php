6b5abd5e216316e1d2b6053216a8bea1e69343b4
jasmguli <nikolalondrovic41@gmail.com>
2022-07-10 17:22:35 +0200

    Added role_id to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('role_id')->constrained('roles');
             $table->string('name');
             $table->string('email')->unique();
-            $table->integer('JMBG');
+            $table->integer('JMBG')->nullable();
+            $table->integer('role_id')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
