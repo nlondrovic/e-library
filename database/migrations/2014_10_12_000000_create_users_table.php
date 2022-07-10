49027d6cd8c410775921ed48cfd8d30240ce4594
jasmguli <nikolalondrovic41@gmail.com>
2022-07-10 17:26:20 +0200

    Removed role_id duplicate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->integer('JMBG')->nullable();
-            $table->integer('role_id')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
