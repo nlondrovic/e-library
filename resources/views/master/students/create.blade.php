b4461b8f6778f5cc5d0fed1c649ea30610dc1efb
jasmguli <nikolalondrovic41@gmail.com>
2022-07-13 23:28:30 +0200

    Made index, show and create views for Student

---
+++ resources/views/master/students/create.blade.php
@@ -0,0 +1,153 @@
+@extends('layouts.app')
+@section('main')
+
+    <!-- Heading of content -->
+    <div class="heading">
+        <div class="pl-[50px] pb-[5px] border-b-[1px] border-[#e4dfdf]">
+            <div>
+                <h1 class="">
+                    New student
+                </h1>
+            </div>
+            <div>
+                <nav class="w-full rounded">
+                    <ol class="flex list-reset">
+                        <li>
+                            <a href="#" class="text-[#2196f3] hover:text-blue-600">
+                                All students
+                            </a>
+                        </li>
+                        <li>
+                            <span class="mx-2">/</span>
+                        </li>
+                        <li>
+                            <a href="#" class="text-gray-400 hover:text-blue-600">
+                                New student
+                            </a>
+                        </li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+
+    <!-- Space for content -->
+    <div class="scroll height-content section-content">
+
+        <form class="text-gray-700 text-[14px] forma" action="{{ route('student.store') }}" method="post">
+            @csrf
+            @method('post')
+            <div class="flex flex-row ml-[50px]">
+                <div class="w-[50%] mb-[100px]">
+
+                    <div class="mt-[20px]">
+                        <span>Name<span class="text-red-500">*</span></span>
+                        <input type="text" name="name" required
+                               class="flex w-[90%] mt-2 px-2 py-2 text-base bg-white border border-gray-300 shadow-sm
+                               appearance-none focus:outline-none focus:ring-2 focus:ring-[#576cdf]"
+                               value="{{ old('name') }}"/>
+                        @if($errors->first('name'))
+                            <span class="text-red-600">{{ $errors->first('name') }}</span>
+                        @endif
+                    </div>
+
+                    {{--                    <div class="mt-[20px]">--}}
+                    {{--                        <span>User type</span>--}}
+                    {{--                        <select--}}
+                    {{--                            class="flex w-[90%] mt-2 px-2 py-2 border bg-gray-300 border-gray-300 shadow-sm--}}
+                    {{--                            focus:outline-none focus:ring-2 focus:ring-[#576cdf]" name="tip_korisnika" disabled--}}
+                    {{--                            required>--}}
+                    {{--                            <option value="">--}}
+                    {{--                                Ucenik--}}
+                    {{--                            </option>--}}
+                    {{--                        </select>--}}
+                    {{--                    </div>--}}
+
+                    {{-- TODO: Kako validirati JMBG npr. 0409003250606, zbog nule na pocetku kaze da nije integer, zato sam stavio da je tip text --}}
+                    <div class="mt-[20px]">
+                        <span>JMBG <span class="text-red-500">*</span></span>
+                        <input type="text" name="jmbg" required
+                               class="flex w-[90%] mt-2 px-2 py-2 text-base bg-white border border-gray-300 shadow-sm
+                               appearance-none focus:outline-none focus:ring-2 focus:ring-[#576cdf]"
+                               value="{{ old('jmbg') }}"/>
+                        @if($errors->first('jmbg'))
+                            <span class="text-red-600">{{ $errors->first('jmbg') }}</span>
+                        @endif
+                    </div>
+
+                    <div class="mt-[20px]">
+                        <span>E-mail <span class="text-red-500">*</span></span>
+                        <input type="text" name="email" required id="studentEmail"
+                               class="flex w-[90%] mt-2 px-2 py-2 text-base bg-white border border-gray-300 shadow-sm
+                               appearance-none focus:outline-none focus:ring-2 focus:ring-[#576cdf]"
+                               value="{{ old('email') }}"/>
+                        @if($errors->first('email'))
+                            <span class="text-red-600" id="studentEmailErrorMsg">{{ $errors->first('email') }}</span>
+                        @endif
+                    </div>
+
+                    <div class="mt-[20px]">
+                        <span>Password <span class="text-red-500">*</span></span>
+                        <input type="password" name="password" required
+                               class="flex w-[90%] mt-2 px-2 py-2 text-base bg-white border border-gray-300 shadow-sm
+                               appearance-none focus:outline-none focus:ring-2 focus:ring-[#576cdf]"/>
+                        @if($errors->first('password'))
+                            <span class="text-red-600">{{ $errors->first('password') }}</span>
+                        @endif
+                    </div>
+
+                    <div class="mt-[20px]">
+                        <span>Repeat password <span class="text-red-500">*</span></span>
+                        <input type="password" name="confirm_password" required
+                               class="flex w-[90%] mt-2 px-2 py-2 text-base bg-white border border-gray-300 shadow-sm
+                               appearance-none focus:outline-none focus:ring-2 focus:ring-[#576cdf]"/>
+                        @if($errors->first('confirm_password'))
+                            <span class="text-red-600">{{ $errors->first('confirm_password') }}</span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="mt-[50px]">
+                    <label class="mt-6 cursor-pointer">
+                        <div class="relative w-48 h-48 py-[48px] text-center border-2 border-gray-300 border-solid">
+                            <div class="py-4">
+                                <svg class="mx-auto feather feather-image mb-[15px]" xmlns="http://www.w3.org/2000/svg"
+                                     width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                     stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
+                                    <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+                                    <circle cx="8.5" cy="8.5" r="1.5"></circle>
+                                    <polyline points="21 15 16 10 5 21"></polyline>
+                                </svg>
+                                <span class="px-4 py-2 mt-2 leading-normal">Add photo</span>
+                                <input type="file" class="hidden" :accept="accept" onchange="loadFileStudent(event)"/>
+                            </div>
+                            <img id="image-output-student" class="hidden absolute w-48 h-[188px] bottom-0"/>
+                        </div>
+                    </label>
+                </div>
+
+            </div>
+
+            {{-- Buttons --}}
+            <div class="absolute bottom-0 w-full">
+                <div class="flex flex-row">
+                    <div class="inline-block w-full text-right py-[7px] mr-[100px] text-white">
+                        <button type="button"
+                                class="btn-animation shadow-lg mr-[15px] w-[150px] focus:outline-none text-sm py-2.5
+                                px-5 transition duration-300 ease-in bg-[#F44336] hover:bg-[#F55549] rounded-[5px]">
+                            Cancel <i class="fas fa-times ml-[4px]"></i>
+                        </button>
+                        <button type="submit"
+                                class="btn-animation shadow-lg w-[150px] disabled:opacity-50 focus:outline-none text-sm
+                                py-2.5 px-5 transition duration-300 ease-in rounded-[5px] hover:bg-[#46A149] bg-[#4CAF50]">
+                            Save <i class="fas fa-check ml-[4px]"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+        </form>
+    </div>
+
+@endsection
+
---
+++ resources/views/master/students/show.blade.php
@@ -0,0 +1,101 @@
+@extends('layouts.app')
+@section('main')
+
+    <!-- Heading of content -->
+    <div class="heading">
+        <div class="flex flex-row  justify-between border-b-[1px] border-[#e4dfdf]">
+            <div class="pl-[50px] py-[10px] flex flex-col">
+                <div>
+                    <h1>
+                        {{ $student->name }}
+                    </h1>
+                </div>
+                <div>
+                    <nav class="w-full rounded">
+                        <ol class="flex list-reset">
+                            <li>
+                                <a href="#" class="text-[#2196f3] hover:text-blue-600">
+                                    All students
+                                </a>
+                            </li>
+                            <li>
+                                <span class="mx-2">/</span>
+                            </li>
+                            <li>
+                                <a href="#" class="text-[#2196f3] hover:text-blue-600">
+                                    ID-{{ $student->id }}
+                                </a>
+                            </li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+            <div class="pt-[24px] pr-[30px]">
+                <a href="#" class="inline hover:text-blue-600 show-modal">
+                    <i class="fas fa-redo-alt mr-[3px]"></i>
+                    Reset password
+                </a>
+                <a href="#" class="hover:text-blue-600 inline ml-[20px] pr-[10px]">
+                    <i class="fas fa-edit mr-[3px] "></i>
+                    Edit student
+                </a>
+                <p class="inline cursor-pointer text-[25px] py-[10px] pl-[30px] border-l-[1px] border-gray-300 dotsStudentProfile hover:text-[#606FC7]">
+                    <i
+                        class="fas fa-ellipsis-v"></i>
+                </p>
+                <div
+                    class="z-10 hidden transition-all duration-300 origin-top-right transform scale-95 -translate-y-2 dropdown-student-profile">
+                    <div
+                        class="absolute right-0 w-56 mt-[10px] origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
+                        aria-labelledby="headlessui-menu-button-1" id="headlessui-menu-items-117" role="menu">
+                        <div class="py-1">
+                            <a href="#" tabindex="0"
+                               class="flex w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 outline-none hover:text-blue-600"
+                               role="menuitem">
+                                <i class="fa fa-trash mr-[5px] ml-[5px] py-1"></i>
+                                <span class="px-4 py-0">Delete student</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="border-b-[1px] py-4 text-gray-500 border-[#e4dfdf] pl-[50px]">
+        <a href="#" class="inline active-book-nav">
+            Basic details
+        </a>
+        <a href="#" class="inline ml-[70px] hover:text-blue-800">
+            Renting history
+        </a>
+    </div>
+    <div class="height-ucenikProfile pb-[30px] scroll">
+        <!-- Space for content -->
+        <div class="pl-[50px] section- mt-[20px]">
+            <div class="flex flex-row">
+                <div class="mr-[30px]">
+                    <div class="mt-[20px]">
+                        <span class="text-gray-500">Ime i prezime</span>
+                        <p class="font-medium">{{ $student->name }}</p>
+                    </div>
+                    <div class="mt-[40px]">
+                        <span class="text-gray-500">User type</span>
+                        <p class="font-medium">{{ $student->role_id }}</p>
+                    </div>
+                    <div class="mt-[40px]">
+                        <span class="text-gray-500">JMBG</span>
+                        <p class="font-medium">{{ $student->JMBG }}</p>
+                    </div>
+                    <div class="mt-[40px]">
+                        <span class="text-gray-500">Email</span>
+                        <a href="#" class="block font-medium text-[#2196f3]">{{ $student->email }}</a>
+                    </div>
+
+                </div>
+                <div class="ml-[100px]  mt-[20px]">
+                    <img class="p-2 border-2 border-gray-300" width="300px" src="{{ $student->picture }}" alt="">
+                </div>
+            </div>
+        </div>
+
+@endsection
