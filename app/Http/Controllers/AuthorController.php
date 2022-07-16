d04785ee0b2d5c377c56eedf4fe1b1ba7943b769
jasmguli <nikolalondrovic41@gmail.com>
2022-07-16 16:27:09 +0200

    Almost finished Author CRUD
    Created views and worked on controller as well as requests of Authors

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -5,96 +5,52 @@ namespace App\Http\Controllers;
 use App\Models\Author;
 use App\Http\Requests\StoreAuthorRequest;
 use App\Http\Requests\UpdateAuthorRequest;
-use http\Env\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthorController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $authors = Author::all();
-
         return view('master.authors.index', compact('authors'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('master.authors.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreAuthorRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreAuthorRequest $request,Author $author)
+    public function store(StoreAuthorRequest $request)
     {
-
-        $inputs = $request->validate([
-           'name' => 'required',
-            'about' => 'min:30',
-        ]);
+        $inputs = $request->validated();
+        $inputs['picture'] = '/assets/img/user.jpg';
         Author::create($inputs);
-        //return dd($request);
+        // TODO: Vidi zasto ne stavlja sliku autoru
+
         return redirect()->route('authors.index');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Author  $author
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function show(Author $author)
     {
-        $author = Author::findOrFail($id);
-
         return view('master.authors.show', compact('author'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Author  $author
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Author $author)
     {
-        //
+        return view('master.authors.edit', compact('author'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateAuthorRequest  $request
-     * @param  \App\Models\Author  $author
-     * @return \Illuminate\Http\Response
-     */
     public function update(UpdateAuthorRequest $request, Author $author)
     {
-        //
+        $inputs = $request->validated();
+        $author->update($inputs);
+
+        return redirect()->route('authors.show', compact('author'));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Author  $author
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Author $author)
     {
         $author->delete();
-
         return redirect()->route('authors.index');
     }
 }
--- resources/views/master/authors/index.blade.php
+++ resources/views/master/authors/index.blade.php
@@ -11,6 +11,8 @@
     </div>
 
     <div class="height-autori pb-[30px] pl-[30px] scroll">
+
+        {{--Create Button--}}
         <div class="flex items-center px-[30px] py-4 space-x-3 rounded-lg justify-between">
             <a href="{{ route('authors.create') }}" class="btn-animation inline-flex items-center text-sm py-2.5 px-5
             transition duration-300 ease-in rounded-[5px] tracking-wider text-white bg-[#3f51b5] rounded hover:bg-[#4558BE]">
@@ -18,8 +20,10 @@
             </a>
         </div>
 
+        {{--Table--}}
         <div
             class="inline-block min-w-full px-[30px] pt-3 align-middle bg-white rounded-bl-lg rounded-br-lg shadow-dashboard">
+
             <table class="overflow-hidden shadow-lg rounded-xl min-w-full border-[1px] border-[#e4dfdf]" id="myTable">
                 <thead class="bg-[#EFF3F6]">
                 <tr class="border-b-[1px] border-[#e4dfdf]">
@@ -35,62 +39,60 @@
                     <th class="px-4 py-4"></th>
                 </tr>
                 </thead>
+
                 <tbody class="bg-white">
-                @foreach($authors as $author)
-                    <tr class="hover:bg-gray-200 hover:shadow-md border-b-[1px] border-[#e4dfdf]">
-                        <td class="px-4 py-3 whitespace-no-wrap">
-                            <label class="inline-flex items-center">
-                                <input type="checkbox" class="form-checkbox">
-                            </label>
-                        </td>
-                        <td class="flex flex-row items-center px-4 py-3">
-                            <img class="object-cover w-8 mr-2 h-11" src="{{ $author->picture }}" alt=""/>
-                            <a href="{{ route('authors.show', $author) }}">
-                                <span class="mr-2 font-medium text-center">{{ $author->name }}</span>
-                            </a>
-                        </td>
-                        </td>
-                        <td class="px-4 py-3 text-sm leading-5 whitespace-no-wrap">{{ Str::limit($author->about )}}</td>
-                        <td class="px-4 py-3 text-sm leading-5 text-right whitespace-no-wrap">
-                            <p class="inline cursor-pointer text-[20px] py-[10px] px-[30px] border-gray-300 dotsAutori hover:text-[#606FC7]">
-                                <i class="fas fa-ellipsis-v"></i>
-                            </p>
-                            <div class="absolute z-10 hidden transition-all duration-300 origin-top-right transform
-                            scale-95 -translate-y-2 dropdown-autori">
-                                <div class="absolute right-[55px] w-56 mt-[7px] origin-top-right bg-white border
-                                border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
-                                     aria-labelledby="headlessui-menu-button-1" id="headlessui-menu-items-117"
-                                     role="menu">
-                                    <div class="py-1">
-                                        <a href="{{ route('authors.show', $author) }}" tabindex="0" class="flex w-full
-                                        px-4 py-2 text-sm leading-5 text-left text-gray-700 outline-none hover:text-blue-600"
-                                           role="menuitem">
-                                            <i class="far fa-file mr-[5px] ml-[5px] py-1"></i>
-                                            <span class="px-4 py-0">View more</span>
-                                        </a>
-                                        <form method="post" action="{{ route('authors.edit', $author) }}">
-                                            <a href="" tabindex="0" class="flex w-full px-4 py-2 text-sm leading-5
-                                            text-left text-gray-700 outline-none hover:text-blue-600"
+                    @foreach($authors as $author)
+                        <tr class="hover:bg-gray-200 hover:shadow-md border-b-[1px] border-[#e4dfdf]">
+                            <td class="px-4 py-3 whitespace-no-wrap">
+                                <label class="inline-flex items-center">
+                                    <input type="checkbox" class="form-checkbox">
+                                </label>
+                            </td>
+                            <td class="flex flex-row items-center px-4 py-3">
+                                <img class="object-cover w-8 mr-2 h-11" src="{{ $author->picture }}" alt=""/>
+                                <a href="{{ route('authors.show', $author) }}">
+                                    <span class="mr-2 font-medium text-center">{{ $author->name }}</span>
+                                </a>
+                            </td>
+                            <td class="px-4 py-3 text-sm leading-5 whitespace-no-wrap">{{ Str::limit($author->about )}}</td>
+                            <td class="px-4 py-3 text-sm leading-5 text-right whitespace-no-wrap">
+                                <p class="inline cursor-pointer text-[20px] py-[10px] px-[30px] border-gray-300 dotsAutori hover:text-[#606FC7]">
+                                    <i class="fas fa-ellipsis-v"></i>
+                                </p>
+                                <div class="absolute z-10 hidden transition-all duration-300 origin-top-right transform
+                                scale-95 -translate-y-2 dropdown-autori">
+                                    <div class="absolute right-[55px] w-56 mt-[7px] origin-top-right bg-white border
+                                    border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
+                                         aria-labelledby="headlessui-menu-button-1" id="headlessui-menu-items-117"
+                                         role="menu">
+                                        <div class="py-1">
+                                            <a href="{{ route('authors.show', $author) }}" tabindex="0" class="flex w-full
+                                            px-4 py-2 text-sm leading-5 text-left text-gray-700 outline-none hover:text-blue-600"
+                                               role="menuitem">
+                                                <i class="far fa-file mr-[5px] ml-[5px] py-1"></i>
+                                                <span class="px-4 py-0">View more</span>
+                                            </a>
+                                            <a href="{{ route('authors.edit', ['author' => $author]) }}" tabindex="0" class="flex w-full px-4 py-2 text-sm leading-5
+                                                text-left text-gray-700 outline-none hover:text-blue-600"
                                                role="menuitem">
                                                 <i class="fas fa-edit mr-[1px] ml-[5px] py-1"></i>
                                                 <span class="px-4 py-0">Edit author</span>
                                             </a>
-                                        </form>
-                                        <form method="post" action="{{ route('authors.destroy', $author) }}">
-                                            @csrf
-                                            @method('delete')
-                                            <button type="submit" tabindex="0" class="flex w-full px-4 py-2 text-sm leading-5
-                                            text-left text-gray-700 outline-none hover:text-blue-600" role="menuitem">
-                                                <i class="fa fa-trash mr-[5px] ml-[5px] py-1"></i>
-                                                <span class="px-4 py-0">Delete author</span>
-                                            </button>
-                                        </form>
+                                            <form method="post" action="{{ route('authors.destroy', $author) }}">
+                                                @csrf
+                                                @method('delete')
+                                                <button type="submit" tabindex="0" class="flex w-full px-4 py-2 text-sm leading-5
+                                                text-left text-gray-700 outline-none hover:text-blue-600" role="menuitem">
+                                                    <i class="fa fa-trash mr-[5px] ml-[5px] py-1"></i>
+                                                    <span class="px-4 py-0">Delete author</span>
+                                                </button>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </td>
-                    </tr>
-                @endforeach
+                            </td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
 
