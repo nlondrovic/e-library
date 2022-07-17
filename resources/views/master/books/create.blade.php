b7c43d9bbdf94ec8c8ebb066c77d278f447e15e6
jasmguli <nikolalondrovic41@gmail.com>
2022-07-17 12:06:01 +0200

    Made views for Book CRUD

--- resources/views/master/books/create.blade.php
+++ resources/views/master/books/create.blade.php
@@ -4,81 +4,57 @@
     <!-- Heading of content -->
     <div class="heading">
         <div class="flex border-b-[1px] border-[#e4dfdf]">
-            <div class="pl-[50px] py-[2px] flex flex-col">
-                <div>
-                    <h1>
-                        New book
-                    </h1>
-                </div>
-                <div>
-                    <nav class="w-full rounded">
-                        <ol class="flex list-reset">
-                            <li>
-                                <a href="" class="text-[#2196f3] hover:text-blue-600">
-                                    All books
-                                </a>
-                            </li>
-                            <li>
-                                <span class="mx-2">/</span>
-                            </li>
-                            <li>
-                                <a href="#" class="text-[#2196f3] hover:text-blue-600">
-                                    New book
-                                </a>
-                            </li>
-                        </ol>
-                    </nav>
-                </div>
+            <div class="pl-[50px] py-[2px] pb-[27px] flex flex-col">
+                <h1>New book</h1>
             </div>
         </div>
     </div>
 
-    {{--            <div class="py-4 text-gray-500 border-b-[1px] border-[#e4dfdf] pl-[30px]">--}}
-    {{--                <a href="#" class="inline active-book-nav hover:text-blue-800">--}}
-    {{--                    Overview--}}
-    {{--                </a>--}}
-    {{--                <a href="novaKnjigaSpecifikacija.php" class="inline ml-[70px] hover:text-blue-800 ">--}}
-    {{--                    Specifications--}}
-    {{--                </a>--}}
-    {{--                <a href="novaKnjigaMultimedija.php" class="inline ml-[70px] hover:text-blue-800">--}}
-    {{--                    Multimedia--}}
-    {{--                </a>--}}
-    {{--            </div>--}}
-
     <!-- Space for content -->
     <div class="scroll height-content px-[30px] section-content">
-        <form class="text-gray-700 forma" method="post" action="{{route('books.store')}}"
+
+        <form class="text-gray-700 forma" method="post" action="{{ route('books.store') }}"
               enctype="multipart/form-data">
             @csrf
             @method('post')
+
             <div class="flex flex-row ml-[30px] mb-[150px]">
+
                 <div class="w-[50%]">
                     <div class="mt-[20px]">
                         <p>Book title<span class="text-red-500">*</span></p>
-                        <input type="text" name="title" id="nazivKnjiga" class="flex w-[90%] mt-2 px-2 py-2 text-base
-                                bg-white border border-gray-300 shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-[#576cdf]"/>
-
+                        <input required type="text" name="title" class="flex w-[90%] mt-2 px-2 py-2 text-base
+                                bg-white border border-gray-300 shadow-sm appearance-none focus:outline-none
+                                focus:ring-2 focus:ring-[#576cdf]" value="{{ old('title') }}"/>
+                        @if($errors->first('title'))
+                            <span class="text-red-600">{{ $errors->first('title') }}</span>
+                        @endif
                     </div>
 
                     <div class="mt-[20px]">
-                        <p class="inline-block mb-2">Content</p>
-                        <textarea name="content" class="flex w-[90%] mt-2 px-2 py-2 text-base bg-white border
-                                border-gray-300 shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-[#576cdf]"></textarea>
+                        <p>Content<span class="text-red-500">*</span></p>
+                        <textarea required name="content" class="flex w-[90%] mt-2 px-2 py-2 text-base bg-white border
+                                border-gray-300 shadow-sm appearance-none focus:outline-none focus:ring-2
+                                focus:ring-[#576cdf]" rows="8">{{ old('content') }}</textarea>
+                        @if($errors->first('content'))
+                            <span class="text-red-600">{{ $errors->first('content') }}</span>
+                        @endif
                     </div>
 
                     <div class="mt-[20px]">
                         <p>ISBN<span class="text-red-500">*</span></p>
-                        <input type="number" name="ISBN" id="ISBN" class="flex w-[90%] mt-2 px-2 py-2 text-base
-                                 bg-white border border-gray-300 shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-[#576cdf]"/>
-
+                        <input required type="number" name="isbn" class="flex w-[90%] mt-2 px-2 py-2 text-base
+                                 bg-white border border-gray-300 shadow-sm appearance-none
+                                 focus:outline-none focus:ring-2 focus:ring-[#576cdf]" value="{{ old('isbn') }}"/>
+                        @if($errors->first('isbn'))
+                            <span class="text-red-600">{{ $errors->first('isbn') }}</span>
+                        @endif
                     </div>
-
                     <div class="w-[50%]">
                         <div class="mt-[20px]">
                             <p>Choose an author <span class="text-red-500">*</span></p>
-                            <select class="flex flex-col w-[90%] flex p-1 my-2 py-2.5 bg-white border border-gray-300
-                            shadow-sm svelte-1l8159u focus-within:ring-2 focus-within:ring-[#576cdf]" id="autori"
-                                    name="author_id">
+                            <select required class="flex flex-col w-[90%] flex p-1 my-2 py-2.5 bg-white border border-gray-300
+                                shadow-sm svelte-1l8159u focus-within:ring-2 focus-within:ring-[#576cdf]" name="author_id">
                                 @foreach ($authors as $author)
                                     <option value="{{ $author->id }}">
                                         {{ $author->name }}
@@ -88,15 +64,12 @@
                         </div>
                     </div>
 
-
+                    {{-- Buttons --}}
                     <div class="absolute bottom-0 w-full">
                         <div class="flex flex-row">
-                            <div
-                                class="inline-block w-full text-white text-right py-[7px] px-5 px-[50px] mr-[100px]">
-
-                                <button type="button"
-                                        class="btn-animation shadow-lg mr-[15px] w-[150px] focus:outline-none
-                                                text-sm py-2.5 px-5 transition duration-300 ease-in bg-[#F44336] hover:bg-[#F55549] rounded-[5px]">
+                            <div class="inline-block w-full text-white text-right py-[7px] px-5 px-[50px] mr-[100px]">
+                                <button type="button" class="btn-animation shadow-lg mr-[15px] w-[150px] focus:outline-none
+                                    text-sm py-2.5 px-5 transition duration-300 ease-in bg-[#F44336] hover:bg-[#F55549] rounded-[5px]">
                                     Cancel <i class="fas fa-times ml-[4px]"></i>
                                 </button>
 
@@ -110,9 +83,30 @@
                     </div>
 
                 </div>
+
+                <div class="mt-[50px] w-[50%]">
+                    <label class="mt-6 cursor-pointer">
+                        <div class="relative w-48 h-48 py-[48px] text-center border-2 border-gray-300 border-solid">
+                            <div class="py-4">
+                                <svg class="mx-auto feather feather-image mb-[15px]" xmlns="http://www.w3.org/2000/svg"
+                                     width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                     stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
+                                    <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+                                    <circle cx="8.5" cy="8.5" r="1.5"></circle>
+                                    <polyline points="21 15 16 10 5 21"></polyline>
+                                </svg>
+                                <span class="px-4 py-2 mt-2 leading-normal">Add photo</span>
+                                <input type="file" name="picture" class="hidden" :accept="accept"
+                                       onchange="loadFileStudent(event)"/>
+                            </div>
+                            <img id="image-output-student" class="absolute w-48 h-[188px] bottom-0"/>
+                        </div>
+                    </label>
+                </div>
             </div>
+
         </form>
-    </div>
+
     </div>
 
 @endsection
--- resources/views/master/books/index.blade.php
+++ resources/views/master/books/index.blade.php
@@ -10,6 +10,7 @@
 
     <!-- Space for content -->
     <div class="scroll height-evidencija">
+        {{-- New Book --}}
         <div class="flex items-center justify-between px-[50px] py-4 space-x-3 rounded-lg">
             <a href="{{ route('books.create') }}"
                class="btn-animation inline-flex items-center text-sm py-2.5 px-5 transition duration-300
@@ -18,7 +19,6 @@
             </a>
         </div>
 
-        <!-- Space for content -->
         <div class="px-[50px] pt-2 bg-white">
             <div class="w-full mt-2">
 
@@ -27,28 +27,17 @@
                     <!-- Table head-->
                     <thead class="bg-[#EFF3F6]">
                     <tr class="border-b-[1px] border-[#e4dfdf]">
-                        <th class="px-4 py-4 leading-4 tracking-wider text-left text-blue-500">
-                            <label class="inline-flex items-center">
-                                <input type="checkbox" class="form-checkbox checkAll">
-                            </label>
-                        </th>
-                        <th class="flex items-center px-4 py-4 leading-4 tracking-wider text-left">
-                            Title
-                        </th>
+                        <th class="flex items-center px-4 py-4 leading-4 tracking-wider text-left">Title</th>
                         <th class="px-4 py-4 text-sm leading-4 tracking-wider text-left">Author</th>
                         <th class="px-4 py-4 text-sm leading-4 tracking-wider text-left">ISBN</th>
                         <th class="px-4 py-4 text-sm leading-4 tracking-wider text-left">Content</th>
                         <th class="px-4 py-4"></th>
                     </tr>
                     </thead>
+                    {{-- Tabe body --}}
                     <tbody class="bg-white">
                     @foreach($books as $book)
                         <tr class="hover:bg-gray-200 hover:shadow-md border-b-[1px] border-[#e4dfdf]">
-                            <td class="px-4 py-4 whitespace-no-wrap">
-                                <label class="inline-flex items-center">
-                                    <input type="checkbox" class="form-checkbox checkOthers">
-                                </label>
-                            </td>
                             <td class="flex flex-row items-center px-4 py-4">
                                 <img class="object-cover w-8 mr-2 h-11" src="{{ $book->picture }}" alt=""/>
                                 <a href="{{ route('books.show', $book) }}">
@@ -56,7 +45,7 @@
                                 </a>
                             </td>
                             <td class="px-4 py-4 text-sm leading-5 whitespace-no-wrap">{{ $book->author->name }}</td>
-                            <td class="px-4 py-4 text-sm leading-5 whitespace-no-wrap">{{ $book->ISBN }}</td>
+                            <td class="px-4 py-4 text-sm leading-5 whitespace-no-wrap">{{ $book->isbn }}</td>
                             <td class="px-4 py-4 text-sm leading-5 whitespace-no-wrap">{{ Str::limit($book->content, 50) }}</td>
                             <td class="px-6 py-4 text-sm leading-5 text-right whitespace-no-wrap">
                                 <p class="inline cursor-pointer text-[20px] py-[10px] px-[30px] border-gray-300 dotsKnjige hover:text-[#606FC7]">
@@ -77,7 +66,7 @@
                                                 <span class="px-4 py-0">Show details</span>
                                             </a>
 
-                                            <a href="{{route('books.edit', $book)}}" tabindex="0"
+                                            <a href="{{ route('books.edit', $book) }}" tabindex="0"
                                                class="flex w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 outline-none hover:text-blue-600"
                                                role="menuitem">
                                                 <i class="fas fa-edit mr-[6px] ml-[5px] py-1"></i>
