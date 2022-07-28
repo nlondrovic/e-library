8d9c39f07b0b3ef2bc66d4a6da4138232df63481
Nikola <nikola1kapor@gmail.com>
2022-07-28 14:49:24 +0200

    Removed default comments

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -5,48 +5,23 @@ namespace App\Http\Controllers;
 use App\Models\Book;
 use App\Models\Reservation;
 use App\Http\Requests\StoreReservationRequest;
-use App\Http\Requests\UpdateReservationRequest;
 use App\Models\User;
 use Carbon\Carbon;
-use http\Env\Request;
-use Illuminate\Support\Facades\Hash;
+use \Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create(\Illuminate\Http\Request $request)
+    public function create(Request $request)
     {
         $students = User::where('role_id', '=', 3)->get();
-
         $book = Book::findOrFail($request->book_id);
 
         return view('master.reservations.create', compact('students', 'book'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreReservationRequest  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(StoreReservationRequest $request)
     {
-//        dd($request->all());
-
         Reservation::query()->create([
             'book_id' => $request->book_id,
             'student_id' => $request->student_id,
@@ -57,50 +32,5 @@ class ReservationController extends Controller
         ]);
 
         return redirect()->route('reservations.index');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Reservation  $reservation
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Reservation $reservation)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Reservation  $reservation
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Reservation $reservation)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateReservationRequest  $request
-     * @param  \App\Models\Reservation  $reservation
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateReservationRequest $request, Reservation $reservation)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Reservation  $reservation
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Reservation $reservation)
-    {
-        //
     }
 }
--- database/migrations/2022_07_25_161054_create_reservation_end_reasons_table.php
+++ database/migrations/2022_07_25_161054_create_reservation_end_reasons_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('reservation_end_reasons', function (Blueprint $table) {
@@ -19,11 +14,6 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('reservation_end_reasons');
