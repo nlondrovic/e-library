d0d222fef5f51670b74e70bad266f39a8c758534
jasmguli <nikolalondrovic41@gmail.com>
2022-08-04 13:59:00 +0200

    Added dashboard elements including chart view

---
+++ resources/views/components/book-chart.blade.php
@@ -0,0 +1,18 @@
+<div class="relative">
+    <h3 class="uppercase mb-[20px] text-left py-[30px]">
+        Book statistics
+    </h3>
+
+    <style rel="stylesheet" type="text/css">
+        .chartBox {
+            width: 600px;
+        }
+    </style>
+
+    <div class="chartBox">
+        <canvas id="myChart"></canvas>
+    </div>
+
+    @include('components.chart-js')
+
+</div>
--- resources/views/components/scripts.blade.php
+++ resources/views/components/scripts.blade.php
@@ -1,4 +1,5 @@
 <script src="{{ asset('assets/js/jquery.min.js') }}" defer></script>
 <script src="{{ asset('assets/js/main.js') }}" defer></script>
 <script src="{{ asset('assets/js/app.js') }}" defer></script>
-<script src="https://cdn.ckeditor.com/4.16.0/standard/ckeditor.js" defer></script>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
