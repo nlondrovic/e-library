b6dc9a67e0fc299ace0865bb29eb12b643fc6da8
jasmguli <nikolalondrovic41@gmail.com>
2022-08-04 14:44:00 +0200

    Fixed dashboard chart not showing

--- resources/views/components/book-chart.blade.php
+++ resources/views/components/book-chart.blade.php
@@ -2,6 +2,7 @@
     <h3 class="uppercase mb-[20px] text-left py-[30px]">
         Book statistics
     </h3>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
     <style rel="stylesheet" type="text/css">
         .chartBox {
@@ -13,6 +14,46 @@
         <canvas id="myChart"></canvas>
     </div>
 
-    @include('components.chart-js')
+    <script>
+        const labels = ['Checkouts', 'Reserved', 'Overdue']
+
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: '# of books',
+                data: [{{ $checkouts }}, {{ $reserved }}, {{ $overdue }}],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                ],
+                borderWidth: 1
+            }]
+        };
+
+        const config = {
+            type: 'bar',
+            data,
+            options: {ticks: {
+                    precision:0
+                },
+                indexAxis: 'y',
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        };
+
+        const myChart = new Chart(document.getElementById('myChart'),
+            config
+        );
+    </script>
 
 </div>
--- resources/views/components/scripts.blade.php
+++ resources/views/components/scripts.blade.php
@@ -1,5 +1,3 @@
 <script src="{{ asset('assets/js/jquery.min.js') }}" defer></script>
 <script src="{{ asset('assets/js/main.js') }}" defer></script>
 <script src="{{ asset('assets/js/app.js') }}" defer></script>
-
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
