544de1d54e5b91000805cf57457bca3383d1ffcc
Nikola <nikola1kapor@gmail.com>
2022-08-08 11:58:37 +0200

    Edited book-chart component

--- resources/views/components/book-chart.blade.php
+++ resources/views/components/book-chart.blade.php
@@ -23,9 +23,9 @@
                 label: '# of books',
                 data: [{{ $checkouts_count }}, {{ $reserved_count }}, {{ $overdue_count }}],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(255, 99, 132, 0.7)',
+                    'rgba(54, 162, 235, 0.7)',
+                    'rgba(255, 206, 86, 0.7)',
                 ],
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
@@ -33,6 +33,7 @@
                     'rgba(255, 206, 86, 1)',
                 ],
                 borderWidth: 1
+
             }]
         };
 
