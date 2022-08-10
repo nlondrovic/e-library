e7865dd03dd68fc4cc35d899dce2d2906e901ece
jasmguli <nikolalondrovic41@gmail.com>
2022-08-10 16:57:22 +0200

    Fixed transactions filter Reset button

--- resources/views/components/scripts.blade.php
+++ resources/views/components/scripts.blade.php
@@ -1,4 +1,16 @@
-<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+{{--<script src="{{ asset('assets/js/jquery.min.js') }}"></script>--}}
 <script src="{{ asset('assets/js/main.js') }}" defer></script>
 <script src="{{ asset('assets/js/app.js') }}" defer></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+<script>
+    $(document).ready(function () {
+        $('.search-select').select2({
+            width: 'resolve',
+            height: 'resolve',
+            placeholder: "Select...",
+            allowClear: true
+        });
+    });
+</script>
