2a72235718483401a0ea66b03256ea0150780cd8
Nikola <nikola1kapor@gmail.com>
2022-08-10 15:50:10 +0200

    Fixed filtering, added autocomplete, fixed the reset button.

--- resources/views/components/scripts.blade.php
+++ resources/views/components/scripts.blade.php
@@ -1,3 +1,4 @@
-<script src="{{ asset('assets/js/jquery.min.js') }}" defer></script>
+<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
 <script src="{{ asset('assets/js/main.js') }}" defer></script>
 <script src="{{ asset('assets/js/app.js') }}" defer></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
     <!-- End Title -->
 
     <!-- Styles -->
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <!-- End Styles -->
