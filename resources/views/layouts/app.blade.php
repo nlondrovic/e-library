c7b370fcc6dc783014ffef13e6e6b23b734b802e
jasmguli <nikolalondrovic41@gmail.com>
2022-08-13 20:39:09 +0200

    Updated font-awesome version

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
 
     <!-- Styles -->
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"/>
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <!-- End Styles -->
 </head>
