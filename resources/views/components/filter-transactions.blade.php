2dc5dd8593ffce957fe43dd85f5a723819d61606
jasmguli <nikolalondrovic41@gmail.com>
2022-08-17 18:29:36 +0200

    Removed duplication of Reset button

--- resources/views/components/filter-transactions.blade.php
+++ resources/views/components/filter-transactions.blade.php
@@ -54,13 +54,5 @@
         </div>
 
     </form>
-    @if(request()->get('student_id') || request()->get('book_id'))
-        <a href="{{ route($route_name) }}">
-            <button class="btn-animation inline-flex items-center text-sm py-2.5 px-5 transition duration-300
-                    ease-in rounded-[5px] tracking-wider text-white bg-[#F44336] rounded hover:bg-[#F55549]">
-                <i class="fas fa-times ml-[4px]"></i>&nbsp;Reset
-            </button>
-        </a>
-    @endif
 
 </div>
