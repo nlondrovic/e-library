2cbe73a1c7227914f56a0d5b91b525e3544c9523
jasmguli <nikolalondrovic41@gmail.com>
2022-08-20 23:33:19 +0200

    Changed min-width for book sidebar

--- resources/views/components/book-sidebar.blade.php
+++ resources/views/components/book-sidebar.blade.php
@@ -1,4 +1,4 @@
-<div class="max-w-xs border-l-[1px] border-[#e4dfdf] ">
+<div class="min-w-[20%] border-l-[1px] border-[#e4dfdf] ">
 @include('components.book-copies')
 @include('components.book-recent-activity')
 </div>
