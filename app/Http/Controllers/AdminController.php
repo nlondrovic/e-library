ce5c594227d01e3553c0f0fb386ff67e4a750b54
Nikola <nikola1kapor@gmail.com>
2022-08-26 21:46:06 +0200

    Admin controller done

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AdminController extends Controller
+{
+    public function index(Request $request)
+    {
+        $admins = User::where('role_id', 1)->orderBy('name', 'asc')->paginate(8);
+        return view('admins.index', compact('admins'));
+    }
+
+    public function create()
+    {
+        return view('admins.create');
+    }
+
+    public function store(StoreUserRequest $request)
+    {
+        $inputs = $request->validated();
+        $inputs['role_id'] = 1;
+        $inputs['password'] = Hash::make($request->password);
+        unset($inputs['confirm_password']);
+
+        if ($request['picture']) {
+            $file = $request['picture'];
+            $filename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+            $filename = "/images/users/" . Str::slug(time() . " " . $filename) . ".{$file->getClientOriginalExtension()}";
+            $inputs['picture'] = $filename;
+            $file->move(public_path('/images/users/'), $filename);
+        } else {
+            $inputs['picture'] = User::DEFAULT_USER_PICTURE_PATH;
+        }
+
+        User::create($inputs);
+        return redirect()->route('admins.index');
+    }
+
+    public function show(User $admin)
+    {
+        return view('admins.show', compact('admin'));
+    }
+
+    public function edit(User $admin)
+    {
+        return view('admins.edit', compact('admin'));
+    }
+
+    public function update(UpdateUserRequest $request, User $admin)
+    {
+        $inputs = $request->validated();
+
+        if ($request['picture']) {
+            $file = $request['picture'];
+            $filename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+            $filename = "/images/users/" . Str::slug(time() . " " . $filename) . ".{$file->getClientOriginalExtension()}";
+            $inputs['picture'] = $filename;
+            $file->move(public_path('/images/users/'), $filename);
+        } else {
+            $inputs['picture'] = User::DEFAULT_USER_PICTURE_PATH;
+        }
+
+        $admin->update($inputs);
+        return redirect()->route("admins.show", compact('admin'));
+    }
+
+    public function destroy(User $admin)
+    {
+        $admin->delete();
+        return redirect()->route("admins.index");
+    }
+}
