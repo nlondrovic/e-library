7a3509051cd10b07e8d33b4e7a01551a416768ca
Nikola <nikola1kapor@gmail.com>
2022-08-28 14:01:59 +0200

    Changed the show page image

--- resources/views/librarians/show.blade.php
+++ resources/views/librarians/show.blade.php
@@ -69,7 +69,7 @@
 
                 </div>
                 <div class="ml-[100px]  mt-[20px]">
-                    <img class="p-2 border-2 border-gray-300" width="300px" src="{{ asset($librarian->picture) }}" alt="Librarian image">
+                    <img class="rounded-full border-2 border-blue-600 p-2" width="300px" src="{{ asset($librarian->picture) }}" alt="Librarian image">
                 </div>
             </div>
         </div>
--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -76,7 +76,7 @@
 
                 </div>
                 <div class="ml-[100px]  mt-[20px]">
-                    <img class="p-2 border-2 border-gray-300" width="300px" src="{{ asset($student->picture) }}" alt="Student image">
+                    <img class="rounded-full border-2 border-blue-600 p-2" width="300px" src="{{ asset($student->picture) }}" alt="Student image">
                 </div>
             </div>
         </div>
