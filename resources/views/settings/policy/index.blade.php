51bd0512d7fd27fc10bb777acf9f31719b2a520a
Nikola <nikola1kapor@gmail.com>
2022-08-31 14:50:25 +0200

    Updated the positioning

--- resources/views/settings/policy/index.blade.php
+++ resources/views/settings/policy/index.blade.php
@@ -19,13 +19,13 @@
                     </div>
                     <div class="relative flex ml-[60px] mt-[20px]">
                         <input type="number" name="holding_time" value="{{ $holding_time }}"
-                               class="h-[50px] flex-1 w-full px-4 py-2 text-sm text-gray-700 placeholder-gray-400
+                               class="h-[50px] flex-1 w-full px-4 py-2 mt-[20px] text-sm text-gray-700 placeholder-gray-400
                                bg-white border-[1px]  border-[#e4dfdf]  rounded-lg shadow-sm appearance-none
                                focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"/>
-                        <p class="ml-[10px] mt-[10px]">{{ __('day(s)') }}</p>
+                        <p class="ml-[10px] mt-[35px]">{{ __('day(s)') }}</p>
                     </div>
                 </div>
-                <div class="flex border-b-[1px] border-[#e4dfdf] pb-[20px] mt-[20px]">
+                <div class="pl-[0px] mt-[20px] flex border-b-[1px] border-[#e4dfdf] pb-[20px]">
                     <div>
                         <h3>{{ __('Reservation time') }}</h3>
                         <p class="pt-[15px] max-w-[400px]">
@@ -38,13 +38,13 @@
                     </div>
                     <div class="relative flex ml-[60px] mt-[20px]">
                         <input type="number" name="reservation_time" value="{{ $reservation_time }}"
-                               class="h-[50px] flex-1 w-full px-4 py-2 text-sm text-gray-700 placeholder-gray-400
+                               class="h-[50px] flex-1 w-full px-4 py-2 mt-[20px] text-sm text-gray-700 placeholder-gray-400
                                bg-white border-[1px]  border-[#e4dfdf]  rounded-lg shadow-sm appearance-none
                                focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"/>
-                        <p class="ml-[10px] mt-[10px]">{{ __('day(s)') }}</p>
+                        <p class="ml-[10px] mt-[35px]">{{ __('day(s)') }}</p>
                     </div>
                 </div>
-                <div class="flex border-b-[1px] border-[#e4dfdf] pb-[20px] mt-[20px]">
+                <div class="pl-[0px] mt-[20px] flex border-b-[1px] border-[#e4dfdf] pb-[20px]">
                     <div>
                         <h3>
                             {{ __('Books per student') }}
@@ -58,10 +58,10 @@
                     </div>
                     <div class="relative flex ml-[60px] mt-[20px]">
                         <input type="number" name="books_per_student" value="{{ $books_per_student }}"
-                               class="h-[50px] flex-1 w-full px-4 py-2 text-sm text-gray-700 placeholder-gray-400
+                               class="h-[50px] flex-1 w-full px-4 py-2 mt-[20px] text-sm text-gray-700 placeholder-gray-400
                                bg-white border-[1px]  border-[#e4dfdf]  rounded-lg shadow-sm appearance-none
                                focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"/>
-                        <p class="ml-[10px] mt-[10px]">{{ __('book(s)') }}</p>
+                        <p class="ml-[10px] mt-[35px]">{{ __('book(s)') }}</p>
                     </div>
                 </div>
                 <button type="submit"
@@ -69,7 +69,6 @@
                             focus:outline-none text-sm py-2.5 px-5 transition duration-300 ease-in rounded-[5px]
                             hover:bg-[#46A149] bg-[#4CAF50]">
                     <i class="fas fa-check mr-[7px]"></i> {{ __('Save') }}
-
                 </button>
             </div>
         </div>
