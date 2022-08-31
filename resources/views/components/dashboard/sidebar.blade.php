90a48e7310814b2288062c410ed85e3624b13e9b
Nikola <nikola1kapor@gmail.com>
2022-08-31 14:50:07 +0200

    Localized the strings, and fixed vertical alignment

--- resources/views/components/dashboard/sidebar.blade.php
+++ resources/views/components/dashboard/sidebar.blade.php
@@ -6,22 +6,24 @@ $route_name = \Illuminate\Support\Facades\Route::currentRouteName();
     <div class="">
         <!-- Hamburger Icon -->
         <div
-            class="cursor-pointer text-[#707070] pl-[30px] pt-[28px] pb-[27px] text-[25px] border-b-[1px] border-[#e4dfdf] ">
+            class="cursor-pointer text-[#707070] pl-[32px] pt-[28px] pb-[27px] text-[25px] border-b-[1px] border-[#e4dfdf] ">
             <i id="hamburger" class="hamburger-btn fas fa-bars"></i>
         </div>
         <div class="mt-[16px]">
             <ul class="text-[#2D3B48] sidebar-nav">
                 <!-- Dashboard Icon -->
-                <li class="pt-[18px] pb-[14px]  group hover:bg-[#EAEAEA]">
-                    <div class="ml-[25px]">
+                <li class="pt-[18px] pb-[14px]  group hover:bg-[#EAEAEA] h-[60px]">
+                    <div class="ml-[30px]">
                         <span class="flex justify-between w-full fill-current whitespace-nowrap">
                             <div class="transition duration-300 ease-in group-hover:text-blue-600">
                                 <a href="{{ route('dashboard') }}" aria-label="Dashboard">
-                                    <i class="px-[5px] pt-[4px] pb-[5px] fa-solid fa-chart-line text-[19px] rounded-[3px] text-[#707070]
+                                        <i class="text-[22px] pt-[4px] pr-1 pb-[5px] fa-solid fa-chart-line rounded-[3px] text-[#707070]
                                         transition duration-300 ease-in group-hover:text-blue-600
                                          @if(str_contains($route_name, 'dashboard')) text-[#576cdf] @endif"></i>
                                     <div class="hidden sidebar-item">
-                                        <p class="inline text-[15px] ml-[15px]">{{ __('Dashboard') }}</p>
+                                        <p class="transition duration-300 ease-in group-hover:text-blue-600 inline text-[15px] ml-[20px]">
+                                             {{ __('Dashboard') }}
+                                        </p>
                                     </div>
                                 </a>
                             </div>
@@ -33,7 +35,7 @@ $route_name = \Illuminate\Support\Facades\Route::currentRouteName();
                     <div class="ml-[30px]">
                         <span class="flex justify-between w-full whitespace-nowrap">
                                 <a href="{{ route('students.index') }}" aria-label="Students">
-                                    <i class="text-[18px] transition duration-300 ease-in group-hover:text-blue-600 text-[#707070] fas fa-users
+                                    <i class="text-[22px] pt-[4px] pb-[5px] transition duration-300 ease-in group-hover:text-blue-600 text-[#707070] fas fa-users
                                          @if(str_contains($route_name, 'students')) text-[#576cdf] @endif"></i>
                                     <div class="hidden sidebar-item">
                                         <p class="transition duration-300 ease-in group-hover:text-blue-600 inline text-[15px] ml-[20px]">
@@ -49,10 +51,10 @@ $route_name = \Illuminate\Support\Facades\Route::currentRouteName();
                     <div class="ml-[30px]">
                         <span class="flex justify-between w-full whitespace-nowrap">
                                 <a href="{{ route('authors.index') }}" aria-label="Authors">
-                                    <i class="text-[25px] transition duration-300 ease-in group-hover:text-blue-600 text-[#707070] fas fa-user-pen
+                                    <i class="text-[22px] pt-[4px] pb-[5px] transition duration-300 ease-in group-hover:text-blue-600 text-[#707070] fas fa-user-pen
                                          @if(str_contains($route_name, 'authors')) text-[#576cdf] @endif"></i>
                                     <div class="hidden sidebar-item">
-                                        <p class="transition duration-300 ease-in group-hover:text-blue-600 inline text-[15px] ml-[20px]">
+                                         <p class="transition duration-300 ease-in group-hover:text-blue-600 inline text-[15px] ml-[20px]">
                                             {{ __('Authors') }}
                                         </p>
                                     </div>
@@ -65,10 +67,10 @@ $route_name = \Illuminate\Support\Facades\Route::currentRouteName();
                     <div class="ml-[30px]">
                         <span class="flex justify-between w-full whitespace-nowrap">
                                 <a href="{{ route('books.index') }}" aria-label="Books">
-                                    <i class="text-[25px] transition duration-300 ease-in group-hover:text-blue-600 text-[#707070] fas fa-book
+                                    <i class="text-[22px] pt-[4px] pb-[5px] pr-2 transition duration-300 ease-in group-hover:text-blue-600 text-[#707070] fas fa-book
                                          @if(str_contains($route_name, 'books')) text-[#576cdf] @endif"></i>
                                     <div class="hidden sidebar-item">
-                                        <p class="transition duration-300 ease-in group-hover:text-blue-600 inline text-[15px] ml-[20px]">
+                                         <p class="transition duration-300 ease-in group-hover:text-blue-600 inline text-[15px] ml-[20px]">
                                             {{ __('Books') }}
                                         </p>
                                     </div>
@@ -81,7 +83,7 @@ $route_name = \Illuminate\Support\Facades\Route::currentRouteName();
                     <div class="ml-[30px]">
                         <span class="flex justify-between w-full whitespace-nowrap">
                                 <a href="{{ route('checkouts.index') }}" aria-label="RentingBooks">
-                                    <i class="text-[22px] transition duration-300 ease-in group-hover:text-blue-600 text-[#707070] fas fa-exchange-alt
+                                     <i class="text-[22px] pt-[4px] pb-[5px] pr-1 text-[#707070] fas fa-exchange-alt transition duration-300 ease-in group-hover:text-blue-600
                                          @if(str_contains($route_name, 'checkouts') || str_contains($route_name, 'checkins') ||
                                             str_contains($route_name, 'lost') || str_contains($route_name, 'overdue') ||
                                             str_contains($route_name, 'reservations')) text-[#576cdf] @endif"></i>
@@ -92,6 +94,7 @@ $route_name = \Illuminate\Support\Facades\Route::currentRouteName();
                                     </div>
                                 </a>
                         </span>
+
                     </div>
                 </li>
                 @if(auth()->user()->isAdmin())
@@ -100,11 +103,11 @@ $route_name = \Illuminate\Support\Facades\Route::currentRouteName();
                         <div class="ml-[30px]">
                         <span class="flex justify-between w-full whitespace-nowrap">
                                 <a href="{{ route('librarians.index') }}" aria-label="Librarians">
-                                    <i class="text-[25px] text-[#707070] fa-solid fa-user-tie transition duration-300 ease-in group-hover:text-blue-600
+                                    <i class="text-[22px] pt-[4px] pb-[5px] pr-2 text-[#707070] fa-solid fa-user-tie transition duration-300 ease-in group-hover:text-blue-600
                                          @if(str_contains($route_name, 'librarians')) text-[#576cdf] @endif"></i>
                                     <div class="hidden sidebar-item">
-                                        <p class="inline text-[15px] ml-[20px] transition duration-300 ease-in group-hover:text-blue-600">
-                                            Librarians
+                                          <p class="transition duration-300 ease-in group-hover:text-blue-600 inline text-[15px] ml-[20px]">
+                                           {{__('Librarians')}}
                                         </p>
                                     </div>
                                 </a>
@@ -116,11 +119,11 @@ $route_name = \Illuminate\Support\Facades\Route::currentRouteName();
                         <div class="ml-[30px]">
                         <span class="flex justify-between w-full whitespace-nowrap">
                                 <a href="{{ route('admins.index') }}" aria-label="RentingBooks">
-                                    <i class="text-[22px] transition duration-300 ease-in group-hover:text-blue-600 text-[#707070] fa-solid fa-user-shield
+                                    <i class="text-[22px] pt-[4px] pb-[5px] transition duration-300 ease-in group-hover:text-blue-600 text-[#707070] fa-solid fa-user-shield
                                          @if(str_contains($route_name, 'admins')) text-[#576cdf] @endif"></i>
                                     <div class="hidden sidebar-item">
-                                        <p class="transition duration-300 ease-in group-hover:text-blue-600 inline text-[15px] ml-[20px]">
-                                            Admin
+                                          <p class="transition duration-300 ease-in group-hover:text-blue-600 inline text-[15px] ml-[20px]">
+                                           Admin
                                         </p>
                                     </div>
                                 </a>
@@ -138,7 +141,7 @@ $route_name = \Illuminate\Support\Facades\Route::currentRouteName();
             <li class="h-[60px] pt-[18px] pb-[14px]">
                 <a href="{{ route('policy.index') }}" aria-label="Settngs" class="ml-[30px]">
                     <span class="whitespace-nowrap">
-                        <i class="transition duration-300 ease-in group-hover:text-[#576cdf] text-[22px] text-[#707070] fas fa-cog
+                        <i class="transition duration-300 ease-in group-hover:text-[#576cdf] text-[22px] pr-1 text-[#707070] fas fa-cog
                                      @if(str_contains($route_name, 'policy')||
                                         str_contains($route_name, 'categories')||
                                         str_contains($route_name, 'genres')||
@@ -147,8 +150,8 @@ $route_name = \Illuminate\Support\Facades\Route::currentRouteName();
                                         str_contains($route_name, 'sizes')||
                                         str_contains($route_name, 'scripts')) text-[#576cdf] @endif"></i>
                         <div class="hidden sidebar-item">
-                            <p class="transition duration-300 ease-in group-hover:text-[#576cdf] inline text-[15px] ml-[20px]">
-                                {{ __('Settings') }}</p>
+                             <p class="transition duration-300 ease-in group-hover:text-blue-600 inline text-[15px] ml-[20px]">
+                                 {{ __('Settings') }}</p>
                         </div>
                     </span>
                 </a>
