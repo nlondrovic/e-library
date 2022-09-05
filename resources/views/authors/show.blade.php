6b0ff046f176a3d5f0565ea6851685112191b633
Nikola Londrovic <nikolalondrovic41@gmail.com>
2022-09-05 23:40:02 +0200

    Fixed positioning in authors.show

--- resources/views/authors/show.blade.php
+++ resources/views/authors/show.blade.php
@@ -10,29 +10,29 @@
                 <a href="{{ route('authors.show', $author) }}">{{ $author->name }}</a>
             </div>
 
-            <div class="pt-[24px] pr-[30px]">
-                <p class="inline cursor-pointer text-[25px] py-[10px] pl-[30px] border-l-[1px] border-gray-300
-                    dotsAutor hover:text-[#606FC7]">
+            <div class="pt-[24px] mr-[30px]">
+                <p class="inline cursor-pointer text-[25px] py-[10px] pl-[30px] border-l-[1px] border-[#e4dfdf]
+                            dotsKnjigaOsnovniDetalji hover:text-[#606FC7]">
                     <i class="fas fa-ellipsis-v"></i>
                 </p>
                 <div class="z-10 hidden transition-all duration-300 origin-top-right transform scale-95 -translate-y-2
-                    dropdown-autor">
-                    <div class="absolute right-0 w-56 mt-[2px] origin-top-right bg-white border border-gray-200 divide-y
-                        divide-gray-100 rounded-md shadow-lg outline-none">
+                    dropdown-knjiga-osnovni-detalji">
+                    <div class="absolute right-0 w-56 mt-[7px] origin-top-right bg-white border border-gray-200
+                        divide-y divide-gray-100 rounded-md shadow-lg outline-none">
                         <div class="py-1">
                             <a href="{{ route('authors.edit', $author) }}" tabindex="0"
                                class="flex w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 outline-none hover:text-blue-600">
                                 <i class="fas fa-edit mr-[1px] ml-[5px] py-1"></i>
                                 <span class="px-4 py-0">{{ __('Edit author') }}</span>
                             </a>
-                            <form method="post" action="{{ route('authors.destroy', $author) }}">
+                            <form action="{{ route('authors.destroy', $author) }}" method="post">
                                 @csrf
                                 @method('delete')
-                                <button type="submit" tabindex="0"
-                                        class="flex w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 outline-none hover:text-blue-600">
+                                <p tabindex="0" class="flex w-full px-4 py-2 text-sm leading-5 text-left
+                                            text-gray-700 outline-none hover:text-blue-600">
                                     <i class="fa fa-trash mr-[5px] ml-[5px] py-1"></i>
-                                    <span class="px-4 py-0">{{ __('Delete author') }}</span>
-                                </button>
+                                    <button type="submit" class="px-4 py-0">{{__('Delete author')}}</button>
+                                </p>
                             </form>
                         </div>
                     </div>
@@ -41,7 +41,8 @@
         </div>
     </div>
 
-    <div class="pl-[50px] height-profile pb-[30px] scroll mt-[20px]">
+    <div class="height-ucenikProfile pb-[30px] pl-[50px] mt-[20px] flex flex-row">
+
         <div class="mr-[30px]">
             <div class="mt-[20px]">
                 <span class="text-gray-500">{{ __('Name') }}</span>
@@ -55,12 +56,11 @@
             </div>
         </div>
 
-        <div class="flex flex-col">
-            <div class="ml-[100px] mt-[20px]">
-                <img class="p-2 border-2 border-gray-300" width="300px" src="{{asset( $author->picture) }}"
-                     alt="{{ __('Author image') }}">
-            </div>
+        <div class="ml-[100px] mt-[20px]">
+            <img class="p-2 border-2 border-gray-300" width="300px" src="{{asset( $author->picture) }}"
+                 alt="{{ __('Author image') }}">
         </div>
+
     </div>
 
 @endsection
