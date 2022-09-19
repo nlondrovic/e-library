5e274ffca7c097baaf377da31336e91bdcc8d4f4
Nikola Londrovic <nikolalondrovic41@gmail.com>
2022-09-19 23:55:43 +0200

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -28,7 +28,7 @@ class UserSeeder extends Seeder
                 'email' => 'nikolalondrovic41@gmail.com',
                 'email_verified_at' => Carbon::now(),
                 'password' => Hash::make('password'),
-                'role_id' => 2,
+                'role_id' => 1,
                 'jmbg' => '1111111111111',
                 'picture' => 'https://ca.slack-edge.com/T01ALTYJSTE-U033HJXL8HM-5bb1c906a7f6-512'
             ],
@@ -37,7 +37,7 @@ class UserSeeder extends Seeder
                 'email' => 'nikola1kapor@gmail.com',
                 'email_verified_at' => Carbon::now(),
                 'password' => Hash::make('password'),
-                'role_id' => 2,
+                'role_id' => 1,
                 'jmbg' => '1111111111111',
                 'picture' => 'https://ca.slack-edge.com/T01ALTYJSTE-U033Y7P17LK-22e4569d0941-512'
             ],
