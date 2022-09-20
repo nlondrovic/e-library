30f9b89599c6e5b31cb9ed2399d149e17408b979
Nikola <nikola1kapor@gmail.com>
2022-09-20 15:28:33 +0200

    Success flash message

---
+++ resources/views/components/flash-success.blade.php
@@ -0,0 +1,4 @@
+<div class="flash_message success">
+    <i class="fa-solid fa-check text-[17px] mr-[8px] justify-center"></i>{{ __('Success!') }}
+    <p class="text-left"> {{ session()->get('flash-success') }} </p>
+</div>
