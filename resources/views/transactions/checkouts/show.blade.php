4257c52183268de07793bd88c1118ebe0c974261
Nikola <nikola1kapor@gmail.com>
2022-09-21 20:18:48 +0200

    Change the layout and names
    Now shows checkout type as heading, removed unnecessary stuff

--- resources/views/transactions/checkouts/show.blade.php
+++ resources/views/transactions/checkouts/show.blade.php
@@ -5,14 +5,22 @@
     <div class="heading">
         <div class="flex flex-row justify-between border-b-[1px] border-[#e4dfdf]">
             <div class="pl-[50px] pb-[5px] header-breadcrumbs">
-                <h1> {{__('Checkout')}}</h1>
+                <h1>
+                    @if($checkout->isCheckout())
+                    Checkout
+                        @elseif($checkout->isCheckin())
+                        Checkin
+                    @else
+                        Book lost
+                    @endif
+                </h1>
                 <a href="{{ route('dashboard') }}">{{ __('Home') }}</a> >
                 <a href="{{ route('checkouts.index') }}">{{ __('Transactions') }}</a> >
                 <a href="{{ route('checkouts.show', $checkout) }}">{{ __('Checkouts') }}</a> >
                 <a href="{{ route('checkouts.show', $checkout) }}">{{ $checkout->id }}</a>
             </div>
 
-            <div class="pt-[15px] mr-[30px] grid grid-cols-2">
+            <div class="pt-[20px] mr-[30px] grid grid-cols-2">
                 @if(!$checkout->end_time)
                     <div class="col">
                         <form action="{{ route('checkIn', $checkout) }}" method="post">
@@ -51,36 +59,32 @@
                             </a>
                         </div>
                         <div class="mt-[40px]">
-                            <span class="text-gray-500 text-[14px]">{{__('Checkout librarian')}}</span>
-                            <a href="{{ route('librarians.show', $checkout->checkout_librarian) }}">
-                                <p class="font-medium text-[#2196f3]">{{ $checkout->checkout_librarian->name }}</p>
-                            </a>
-                        </div>
-                        <div class="mt-[40px]">
                             <a href="{{ route('students.show', $checkout->student) }}">
                                 <span class="text-gray-500 text-[14px]">{{__('Checkout student')}}</span>
                                 <p class="font-medium text-[#2196f3]">{{ $checkout->student->name }}</p>
                             </a>
                         </div>
                         <div class="mt-[40px]">
-                            <span class="text-gray-500 text-[14px]">{{__('Checkout time')}}</span>
-                            <p class="font-medium">{{ format_time($checkout->start_time) }}</p>
+                            <span class="text-gray-500 text-[14px]">{{__('Checkout librarian')}}</span>
+                            <a href="{{ route('librarians.show', $checkout->checkout_librarian) }}">
+                                <p class="font-medium text-[#2196f3]">{{ $checkout->checkout_librarian->name }}</p>
+                            </a>
                         </div>
                     </div>
 
                     <div class="ml-[50px]">
+                        <div class="mt-[20px]">
+                            <span class="text-gray-500 text-[14px]">{{__('Checkout time')}}</span>
+                            <p class="font-medium">{{ format_time($checkout->start_time) }}</p>
+                        </div>
                         @if($checkout->end_time)
                             @if($checkout->end_reason->id == 2)
-                                <div class="mt-[20px]">
-                                    <span class="text-gray-500 text-[14px]">{{__('Checkin reason')}}</span>
-                                    <p class="font-medium">{{ __($checkout->end_reason->value) }}</p>
-                                </div>
                                 <div class="mt-[40px]">
                                     <span class="text-gray-500 text-[14px]">{{ __('Write-off time') }}</span>
                                     <p class="font-medium">{{ format_time($checkout->end_time) }}</p>
                                 </div>
                             @else
-                                <div class="mt-[20px]">
+                                <div class="mt-[40px]">
                                     <span class="text-gray-500 text-[14px]">{{__('Checkin time')}}</span>
                                     <p class="font-medium">{{ format_time($checkout->end_time) }}</p>
                                 </div>
@@ -90,21 +94,9 @@
                                         <p class="font-medium text-[#2196f3]">{{ $checkout->checkin_librarian->name }}</p>
                                     </a>
                                 </div>
-                                <div class="mt-[40px]">
-                                    <span class="text-gray-500 text-[14px]">{{__('Holding for')}}</span>
-                                    <p class="font-medium">{{ $checkout->getHoldingTime() }}</p>
-                                </div>
-                                <div class="mt-[40px]">
-                                    <span class="text-gray-500 text-[14px]">{{__('Overdue time')}}</span>
-                                    <p class="font-medium">{{ $checkout->getOverdueTime() }}</p>
-                                </div>
-                                <div class="mt-[40px]">
-                                    <span class="text-gray-500 text-[14px]">{{__('Checkin reason')}}</span>
-                                    <p class="font-medium">{{ __($checkout->end_reason->value) }}</p>
-                                </div>
                             @endif
                         @else
-                            <div class="mt-[20px]">
+                            <div class="mt-[40px]">
                                 <span class="text-gray-500 text-[14px]">{{__('Holding for')}}</span>
                                 <p class="font-medium">{{ $checkout->getHoldingTime() }}</p>
                             </div>
