9438d8c7f30b1d0cb4ee3003bab4e3b743297b7a
Nikola Londrovic <nikolalondrovic41@gmail.com>
2022-09-24 17:41:43 +0200

    Removed sending filters to another route in transactions

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -17,7 +17,7 @@ $route_name = \Illuminate\Support\Facades\Route::currentRouteName();
                         <span class="whitespace-nowrap w-full text-[25px] flex justify-between fill-current">
                             <div class="group hover:bg-[#EFF3F6] py-[15px] px-[20px] w-[268px] rounded-[10px] cursor-pointer
                                  @if(str_contains($route_name, 'checkouts')) bg-[#EFF3F6] text-[#576cdf] @endif">
-                                <a href="{{ route('checkouts.index', ['student_ids' => request()->get('student_ids'), 'book_ids' => request()->get('book_ids')]) }}"
+                                <a href="{{ route('checkouts.index') }}"
                                    class="flex items-center">
                                     <i class="@if(str_contains($route_name, 'checkouts')) text-[#576cdf] @else text-[#707070] @endif transition duration-300 ease-in group-hover:text-[#576cdf] far fa-copy text-[20px]"></i>
                                     <p class="transition duration-300 ease-in group-hover:text-[#576cdf] text-[15px] ml-[18px]">
@@ -33,7 +33,7 @@ $route_name = \Illuminate\Support\Facades\Route::currentRouteName();
                         <span class="whitespace-nowrap w-full text-[25px] flex justify-between fill-current">
                             <div class="group hover:bg-[#EFF3F6] py-[15px] px-[20px] w-[268px] rounded-[10px] cursor-pointer
                                  @if(str_contains($route_name, 'checkins')) bg-[#EFF3F6] text-[#576cdf] @endif">
-                                <a href="{{ route('checkins.index', ['student_ids' => request()->get('student_ids'), 'book_ids' => request()->get('book_ids')]) }}"
+                                <a href="{{ route('checkins.index') }}"
                                    class="flex items-center">
                                     <i class="transition duration-300 ease-in @if(str_contains($route_name, 'checkins')) text-[#576cdf] @else text-[#707070] @endif text-[20px] fas fa-file group-hover:text-[#576cdf]"></i>
                                     <p class="transition duration-300 ease-in text-[15px] ml-[21px] group-hover:text-[#576cdf]">
@@ -50,7 +50,7 @@ $route_name = \Illuminate\Support\Facades\Route::currentRouteName();
                         <span class=" whitespace-nowrap w-full text-[25px] flex justify-between fill-current">
                             <div class="group hover:bg-[#EFF3F6] py-[15px] px-[20px] w-[268px] rounded-[10px] cursor-pointer
                                  @if(str_contains($route_name, 'overdue')) bg-[#EFF3F6] text-[#576cdf] @endif">
-                                <a href="{{ route('overdue.index', ['student_ids' => request()->get('student_ids'), 'book_ids' => request()->get('book_ids')]) }}"
+                                <a href="{{ route('overdue.index') }}"
                                    class="flex items-center">
                                     <i class="group-hover:text-[#576cdf] @if(str_contains($route_name, 'overdue')) text-[#576cdf] @else text-[#707070] @endif text-[20px] fas fa-exclamation-triangle transition duration-300 ease-in "></i>
                                     <p class="text-[15px] ml-[17px] transition duration-300 ease-in group-hover:text-[#576cdf]">
@@ -66,7 +66,7 @@ $route_name = \Illuminate\Support\Facades\Route::currentRouteName();
                         <span class=" whitespace-nowrap w-full text-[25px] flex justify-between fill-current">
                             <div class="group hover:bg-[#EFF3F6] py-[15px] px-[20px] w-[268px] rounded-[10px] cursor-pointer
                                  @if(str_contains($route_name, 'lost')) bg-[#EFF3F6] text-[#576cdf] @endif">
-                                <a href="{{ route('lost.index', ['student_ids' => request()->get('student_ids'), 'book_ids' => request()->get('book_ids')]) }}"
+                                <a href="{{ route('lost.index') }}"
                                    class="flex items-center">
                                     <i class="group-hover:text-[#576cdf] @if(str_contains($route_name, 'lost')) text-[#576cdf] @else text-[#707070] @endif text-[20px] fas fa-exclamation-circle transition duration-300 ease-in "></i>
                                     <p class="text-[15px] ml-[17px] transition duration-300 ease-in group-hover:text-[#576cdf]">
@@ -83,7 +83,7 @@ $route_name = \Illuminate\Support\Facades\Route::currentRouteName();
                         <span class="whitespace-nowrap w-full text-[25px] flex justify-between fill-current">
                             <div class="group hover:bg-[#EFF3F6] py-[15px] px-[20px] w-[268px] rounded-[10px] cursor-pointer
                                  @if(str_contains($route_name, 'active')) bg-[#EFF3F6] text-[#576cdf] @endif">
-                                <a href="{{ route('reservations.active', ['student_ids' => request()->get('student_ids'), 'book_ids' => request()->get('book_ids')]) }}"
+                                <a href="{{ route('reservations.active') }}"
                                    class="flex items-center">
                                     <i class="group-hover:text-[#576cdf] @if(str_contains($route_name, 'active')) text-[#576cdf] @else text-[#707070] @endif text-[20px] far fa-calendar-check transition duration-300 ease-in"></i>
                                     <p class="text-[15px] ml-[19px] transition duration-300 ease-in group-hover:text-[#576cdf]">
@@ -99,7 +99,7 @@ $route_name = \Illuminate\Support\Facades\Route::currentRouteName();
                         <span class=" whitespace-nowrap w-full text-[25px] flex justify-between fill-current">
                             <div class="group hover:bg-[#EFF3F6] py-[15px] px-[20px] w-[268px] rounded-[10px] cursor-pointer
                                  @if(str_contains($route_name, 'archived')) bg-[#EFF3F6] text-[#576cdf] @endif">
-                                <a href="{{ route('reservations.archived', ['student_ids' => request()->get('student_ids'), 'book_ids' => request()->get('book_ids')]) }}"
+                                <a href="{{ route('reservations.archived') }}"
                                    class="flex items-center">
                                     <i class="@if(str_contains($route_name, 'archived')) text-[#576cdf] @else text-[#707070] @endif text-[20px] fas fa-calendar-alt transition duration-300 ease-in group-hover:text-[#576cdf]"></i>
                                     <p class="text-[15px] ml-[19px] transition duration-300 ease-in group-hover:text-[#576cdf]">
