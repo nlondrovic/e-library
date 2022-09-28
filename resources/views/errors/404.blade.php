847212aa0ba37f396e22ea2df2fcfa685659e7c7
Nikola Londrovic <nikolalondrovic41@gmail.com>
2022-09-28 23:13:18 +0200

    Update 404.blade.php

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,10 +6,10 @@
             <div class="" style="color:white; position: absolute; top:45%; left: 30px; font-size: 25px; width: 33%">
                 <h2 style="font-size: 90px; color: white"> 404 </h2>
                 <h2 style="font-size: 35px"> {{ __('Whoops! Illegal move.') }} </h2>
-                <h2> {{ __('Stranica kojoj pokusavate pristupiti trenutno nedostupna ili ne postoji.') }} </h2>
+                <h2> {{ __("The page you're trying to access is currently unavailable or doesn't exist.") }} </h2>
                 <div class="" style="font-size: 30px; margin-top: 50px">
-                    <label class=""> <u> {{ __('Click me to go back') }} ></u> </label>
                     <a href="{{ url()->previous() }}" style="color: wheat">
+                    <label class="cursor-pointer"> <u class="mr-[15px]" style=" font-size: 30px"> {{ __('Click me to go back') }} ></u> </label>
                         <i class="fa-solid fa-chess-knight" style="font-size: 45px"></i>
                     </a>
                 </div>
