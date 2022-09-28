a5ba17d7be0e5efa3711adaf8d14ea93bcea1016
Nikola <nikola1kapor@gmail.com>
2022-09-28 22:32:27 +0200

    Had a problem with sizing, so swapped and edited some stuff

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,12 +5,14 @@
 {{--            <img src="{{ asset('assets/img/wood-pattern.jpg') }}" alt="" width="1000" height="1000">--}}
             <img src="{{ asset('assets/img/chess-table.png') }}" class="" style="position: relative; left: -20%;" width="800px" height="800px" alt="">
             <div class="" style="color:white; position: absolute; top:45%; left: 30px; font-family: 'Javanese Text', cursive; font-size: 25px">
-                <h1 style="font-size: 100px; color: white"> 404 </h1>
+                <h2 style="font-size: 90px; color: white"> 404 </h2>
                 <h2 style="font-size: 35px"> {{ __('Whoops! Illegal move.') }} </h2>
                 <h2> {{ __('Stranica kojoj pokusavate pristupiti trenutno nedostupna ili ne postoji.') }} </h2>
-                <div style="font-size: 35px; margin-top: 50px">
-                    <label class=""> {{ __('Click me to go back') }} ></label>
-                    <a class="" href="{{ url()->previous() }}" style="font-size:50px; color: wheat"><i class="fa-solid fa-chess-knight"></i></a>
+                <div style="font-size: 30px; margin-top: 50px">
+                    </u> <label class=""> <u> {{ __('Click me to go back') }} ></u> </label>
+                    <a href="{{ url()->previous() }}" style="color: wheat">
+                        <i class="fa-solid fa-chess-knight" style="font-size: 45px"></i>
+                    </a>
                 </div>
             </div>
         </div>
