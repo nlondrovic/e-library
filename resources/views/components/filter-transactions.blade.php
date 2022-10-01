d1b139e966feab793c9c66cc6b5f1f0321e6112e
Nikola Londrovic <nikolalondrovic41@gmail.com>
2022-10-01 19:52:05 +0200

    Changed order of filter inputs in transactions

--- resources/views/components/filter-transactions.blade.php
+++ resources/views/components/filter-transactions.blade.php
@@ -3,96 +3,129 @@
           class="mb-[20px] mr-[15px]">
         <div class="flex flex-col">
             <div class="flex flex-row items-center">
-            <label class="px-2 mr-[5px]">
-                <p class="text-[15px]">{{ __('Student') }}</p>
-                <select class="flex flex-col h-[50px] p-1 my-2 py-2.5 bg-white border border-gray-300
+
+                {{-- Book --}}
+                <label class="px-2 mr-2">
+                    <p class="text-[15px]">{{ __('Book(s)') }}</p>
+                    <select class="flex flex-col p-1 my-2 py-2.5 bg-white border border-gray-300
                     shadow-sm svelte-1l8159u focus-within:ring-2 focus-within:ring-[#576cdf] search-select-multiple w-[300px]"
-                        name="student_ids[]" multiple="multiple">
-                    @foreach ($students as $student)
-                        <option
-                            @if(request()->student_ids)
-                                @if(in_array($student->id, request()->student_ids)) selected @endif
-                            @endif
-                            value="{{ $student->id }}">
-                            {{ $student->name }}
-                        </option>
-                    @endforeach
-                </select>
-            </label>
-            <label class="px-2 mr-2">
-                <p class="text-[15px]">{{ __('Book') }}</p>
-                <select class="flex flex-col p-1 my-2 py-2.5 bg-white border border-gray-300
+                            id="btn-reset-book" name="book_ids[]" multiple="multiple">
+                        @foreach($books as $book)
+                            <option
+                                @if(request()->book_ids)
+                                    @if(in_array($book->id, request()->book_ids)) selected @endif
+                                @endif
+                                value="{{ $book->id }}">
+                                {{ $book->title }}
+                            </option>
+                        @endforeach
+                    </select>
+                </label>
+
+                {{-- Student --}}
+                <label class="px-2 mr-[5px]">
+                    <p class="text-[15px]">{{ __('Student(s)') }}</p>
+                    <select class="flex flex-col h-[50px] p-1 my-2 py-2.5 bg-white border border-gray-300
                     shadow-sm svelte-1l8159u focus-within:ring-2 focus-within:ring-[#576cdf] search-select-multiple w-[300px]"
-                        id="btn-reset-book" name="book_ids[]" multiple="multiple">
-                    @foreach($books as $book)
-                        <option
-                        @if(request()->book_ids)
-                            @if(in_array($book->id, request()->book_ids)) selected @endif
-                        @endif
-                        value="{{ $book->id }}">
-                            {{ $book->title }}
-                        </option>
-                    @endforeach
-                </select>
-            </label>
-            <label class="px-2">
-                @if(\Request::is('*/activeReservations') || \Request::is('*/archivedReservations'))
-                    <p class="text-[15px]">{{ __('Librarian') }}</p>
-                @else
-                    <p class="text-[15px]">{{ __('Checkout librarian') }}</p>
+                            name="student_ids[]" multiple="multiple">
+                        @foreach ($students as $student)
+                            <option
+                                @if(request()->student_ids)
+                                    @if(in_array($student->id, request()->student_ids)) selected @endif
+                                @endif
+                                value="{{ $student->id }}">
+                                {{ $student->name }}
+                            </option>
+                        @endforeach
+                    </select>
+                </label>
+
+                {{-- Checkout librarian --}}
+                <label class="px-2">
+                    @if(\Request::is('*/activeReservations') || \Request::is('*/archivedReservations'))
+                        <p class="text-[15px]">{{ __('Librarian(s)') }}</p>
+                    @else
+                        <p class="text-[15px]">{{ __('Checkout librarian(s)') }}</p>
+                    @endif
+                    <select class="flex flex-col h-[50px] flex p-1 my-2 py-2.5 bg-white border border-gray-300
+                        shadow-sm svelte-1l8159u focus-within:ring-2 focus-within:ring-[#576cdf] search-select-multiple w-[300px]"
+                            name="checkout_librarian_ids[]" multiple="multiple" style="width: 270px">
+                        @foreach ($checkout_librarians as $librarian)
+                            <option
+                                @if(request()->checkout_librarian_ids)
+                                    @if(in_array($librarian->id, request()->checkout_librarian_ids)) selected @endif
+                                @endif
+                                value="{{ $librarian->id }}">
+                                {{ $librarian->name }}
+                            </option>
+                        @endforeach
+                    </select>
+                </label>
+
+                {{-- Reset filters button --}}
+                @if(filtering() && (\Request::is('*/checkins') || \Request::is('*/lost')))
+                    <a href="{{ route(Route::currentRouteName()) }}"
+                       class="ml-[15px] mt-[20px] btn-animation inline-flex items-center text-sm py-2.5 px-5 transition duration-300
+                                        ease-in rounded-[5px] tracking-wider text-white bg-[#F44336] rounded hover:bg-[#F55549]">
+                        <i class="fas fa-times ml-[4px]"></i>&nbsp;{{ __('Reset filters') }}
+                    </a>
                 @endif
-                <select class="flex flex-col h-[50px] flex p-1 my-2 py-2.5 bg-white border border-gray-300
-                    shadow-sm svelte-1l8159u focus-within:ring-2 focus-within:ring-[#576cdf] search-select-multiple w-[300px]"
-                        name="checkout_librarian_ids[]" multiple="multiple" style="width: 270px">
-                    @foreach ($checkout_librarians as $librarian)
-                        <option
-                            @if(request()->checkout_librarian_ids)
-                                @if(in_array($librarian->id, request()->checkout_librarian_ids)) selected @endif
-                            @endif
-                        value="{{ $librarian->id }}">
-                            {{ $librarian->name }}
-                        </option>
-                    @endforeach
-                </select>
-            </label>
-            @if(\Request::is('*/checkins') || \Request::is('*/overdue') || \Request::is('*/lost'))
-            <label class="px-2 mr-2">
-                <p class="text-[15px]">{{ __('Checkin librarian') }}</p>
-                <select class="flex flex-col h-[50px] flex p-1 my-2 py-2.5 bg-white border border-gray-300
-                    shadow-sm svelte-1l8159u focus-within:ring-2 focus-within:ring-[#576cdf] search-select-multiple w-[300px]"
-                        name="checkin_librarian_ids[]" multiple="multiple" style="width: 270px">
-                    @foreach ($checkin_librarians as $librarian)
-                        <option
-                            @if(request()->checkin_librarian_ids)
-                                @if(in_array($librarian->id, request()->checkin_librarian_ids)) selected @endif
-                            @endif
-                        value="{{ $librarian->id }}">
-                            {{ $librarian->name }}
-                        </option>
-                    @endforeach
-                </select>
-            </label>
-            @endif
+
             </div>
+
             <div class="flex flex-row">
+
+                {{-- Date (from) --}}
                 <div class="px-2 mt-[20px] mr-[14px]">
-                    <label class="text-[15px]">{{ __('Date (from)') }}</label>
-                    <input type="date" name="start_time" class="flex w-[300px] mt-2 px-2 py-2 text-base bg-white border
+                    <p class="text-[15px]">{{ __('Date (from)') }}</p>
+                    <input type="date" name="start_time" class="flex w-[300px] px-2 py-2 text-base bg-white border
                             border-gray-300 shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-[#576cdf]"
                            value="{{ request()->get('start_time') }}"/>
                 </div>
+
+                {{-- Date (to) --}}
                 <div class="mt-[20px]">
-                    <label class="text-[15px]">{{ __('Date (to)') }}</label>
-                    <input type="date" name="end_time" class="flex w-[300px] mt-2 px-2 py-2 text-base bg-white border
+                    <p class="text-[15px]">{{ __('Date (to)') }}</p>
+                    <input type="date" name="end_time" class="flex w-[300px]  px-2 py-2 text-base bg-white border
                             border-gray-300 shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-[#576cdf]"
                            value="{{ request()->get('end_time') }}"/>
                 </div>
-                <div class="px-2 py-[3px] mt-[50px] ml-[15px]">
+
+                @if(\Request::is('*/checkins') || \Request::is('*/lost'))
+                    {{-- Write-off/Checkin librarian --}}
+                    <label class="px-2 mt-[30px] ml-[15px]">
+                        @if(\Request::is('*/lost'))
+                            <p class="text-[15px]">{{ __('Write-off librarian(s)') }}</p>
+                        @else
+                            <p class="text-[15px]">{{ __('Checkin librarian(s)') }}</p>
+                        @endif
+                        <select class="flex flex-col h-[50px] flex p-1 my-2 py-2.5 bg-white border border-gray-300
+                            shadow-sm svelte-1l8159u focus-within:ring-2 focus-within:ring-[#576cdf] search-select-multiple w-[300px]"
+                                name="checkin_librarian_ids[]" multiple="multiple" style="width: 270px">
+                            @foreach ($checkin_librarians as $librarian)
+                                <option
+                                    @if(request()->checkin_librarian_ids)
+                                        @if(in_array($librarian->id, request()->checkin_librarian_ids)) selected @endif
+                                    @endif
+                                    value="{{ $librarian->id }}">
+                                    {{ $librarian->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </label>
+                @endif
+
+                {{-- Buttons --}}
+                <div class="py-[3px] mt-[40px] ml-[15px]">
+
+                    {{-- Filter buttons --}}
                     <button type="submit" class="btn-animation inline-flex items-center text-sm py-2.5 px-5 transition duration-300
                         ease-in rounded-[5px] tracking-wider text-white bg-[#3f51b5] rounded hover:bg-[#4558BE]">
                         <i class="fas fa-search"></i>&nbsp;{{ __('Filter') }}
                     </button>
-                    @if(filtering())
+
+                    {{-- Reset filters button --}}
+                    @if(filtering() && !(\Request::is('*/checkins') || \Request::is('*/lost')))
                         <a href="{{ route(Route::currentRouteName()) }}"
                            class="ml-[15px] btn-animation inline-flex items-center text-sm py-2.5 px-5 transition duration-300
                                         ease-in rounded-[5px] tracking-wider text-white bg-[#F44336] rounded hover:bg-[#F55549]">
@@ -100,6 +133,7 @@
                         </a>
                     @endif
                 </div>
+
             </div>
         </div>
     </form>
