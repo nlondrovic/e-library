1df1270696fedf2f71cb2c9b19adc015eee2ae09
Nikola <nikola1kapor@gmail.com>
2022-10-03 21:46:39 +0200

    Edited colors and strings a bit

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,12 +4,12 @@
         <div class="w-[50%] m-auto">
             <img src="{{ asset('assets/img/chess-table.png') }}" class="" style="position: relative; left: -20%;" width="800px" height="800px" alt="">
             <div class="w-[360px] size-down" style="color:white; position: absolute; top:45%; left: 30px; font-size: 25px; width: 33%">
-                <h2 style="font-size: 75px; color: white"> 404 </h2>
-                <h2 style="font-size: 30px"> {{ __('Whoops! Illegal move.') }} </h2>
+                <h2 style="font-size: 75px; color: white"> 404! </h2>
+                <h2 style="font-size: 30px"> {{ __('Whoops! Something went wrong.') }} </h2>
                 <h2> {{ __("The page you're trying to access is currently unavailable or doesn't exist.") }} </h2>
                 <div class="" style="font-size: 30px; margin-top: 50px">
-                    <a href="{{ url()->previous() }}" style="color: wheat">
-                    <label class="cursor-pointer"> <u class="mr-[15px]" style=" font-size: 30px"> {{ __('Click me to go back') }} ></u> </label>
+                    <a href="{{ url()->previous() }}" style="color: lightblue">
+                    <label class="cursor-pointer"> <span class="mr-[15px]" style=" font-size: 30px"> {{ __('Click me to go back') }} ></span> </label>
                         <i class="fa-solid fa-chess-knight" style="font-size: 45px"></i>
                     </a>
                 </div>
